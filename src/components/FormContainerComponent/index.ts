309f3ea4e18e4a0bc9e17d53744654bb1c44a0dc
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2023-02-20 13:59:20 +0500

    signin page with route

--- src/components/FormContainerComponent/index.ts
+++ src/components/FormContainerComponent/index.ts
@@ -1,14 +1,14 @@
-import Block from '../../core/block/Block';
+import Block from "../../core/block/Block";
 
 export default class FormContainer extends Block {
-  static componentName = 'FormContainer';
+  static componentName = "FormContainer";
 
   constructor({ data }: any) {
     super({ data: JSON.parse(data) });
     this.setProps({
       onClick: (e: Event) => this.handleAuth(e),
       onBlur: this.handleBlur.bind(this),
-      error: '',
+      error: "",
     });
   }
 
@@ -20,10 +20,10 @@ export default class FormContainer extends Block {
       items[key] = value.children[1] as HTMLInputElement;
     }
     for (const [key, value] of Object.entries(items)) {
-      if (this.props.error === '' && value.value === '') {
+      if (this.props.error === "" && value.value === "") {
         this.setProps({
           ...this.props,
-          error: 'Заполните поля',
+          error: "Заполните поля",
         });
       }
     }
@@ -37,18 +37,18 @@ export default class FormContainer extends Block {
       items[key] = value.children[1] as HTMLInputElement;
     }
 
-    if (this.props.error !== '') {
+    if (this.props.error !== "") {
       this.setProps({
         ...this.props,
-        error: '',
+        error: "",
       });
     }
 
     for (const [key, value] of Object.entries(items)) {
-      if (this.props.error === '' && value.value === '') {
+      if (this.props.error === "" && value.value === "") {
         this.setProps({
           ...this.props,
-          error: 'Заполните поля',
+          error: "Заполните поля",
         });
       }
     }
@@ -56,34 +56,28 @@ export default class FormContainer extends Block {
 
   render() {
     return `
-      <div class='form-container-wrapper'>
-        <div class='form-container'>
-          <h2 class='form-container__header'>{{data.header}}</h2>
-          <form class='form-container__form'>
-            {{{ Input
-                id="login"
-                fieldName="login"
-                fieldType="text"
-                fieldLabel="Логин"
-                ref="loginInput"
-                onFocus=onFocus
-                onBlur=onBlur
-            }}}
-            {{{ Input
-              id="password"
-              fieldName="password"
-              fieldType="password"
-              fieldLabel="Пароль"
-              ref="passwordInput"
-              onFocus=onFocus
-              onBlur=onBlur
-            }}}
-            <p>${this.props.error}</p>
-            {{{ Button text=data.button.text className='u-margin-top-big' onClick=onClick}}}
-          </form>
-          {{{ ButtonLink text=data.subButton className='form-container__link'}}}
-        </div>
-      </div>
+      <form class='form-container__form'>
+        {{{ Input
+            id="login"
+            fieldName="login"
+            fieldType="text"
+            fieldLabel="Логин"
+            ref="loginInput"
+            onFocus=onFocus
+            onBlur=onBlur
+        }}}
+        {{{ Input
+          id="password"
+          fieldName="password"
+          fieldType="password"
+          fieldLabel="Пароль"
+          ref="passwordInput"
+          onFocus=onFocus
+          onBlur=onBlur
+        }}}
+        <p>${this.props.error}</p>
+        {{{ Button text=data.button.text className='u-margin-top-big' onClick=onClick}}}
+      </form>
     `;
   }
 }
--- src/pages/SignInPage/index.ts
+++ src/pages/SignInPage/index.ts
@@ -1,38 +1,61 @@
-import Block from '../../core/block/Block';
+import Block from "../../core/block/Block";
+import { CoreRouter } from "core/router/types";
+import withRouter from "utils/HOCS/withRouter";
 
-export default class SignInPage extends Block {
-  static componentName = 'SignInPage';
+type SignInPageProps = {
+  router: CoreRouter;
+  onClick?: (e: Event) => void;
+};
+class SignInPage extends Block<SignInPageProps> {
+  static componentName = "SignInPage";
 
-  constructor() {
-    super();
+  constructor(props: SignInPageProps) {
+    super(props);
+
+    this.setProps({
+      ...this.props,
+      onClick: (e: Event) => this.goToSignUp(e),
+    });
+  }
+
+  goToSignUp(e: Event) {
+    e.preventDefault();
+    this.props.router.go("/signup");
   }
 
   render(): string {
     return `
       <div class='container'>
-        {{{ FormContainer data='{
-          "header": "Вход",
-          "fields": [
-            {
-              "fieldLabel": "Логин",
-              "fieldName": "login",
-              "fieldType": "text",
-              "ref": "loginInput"
-            },
-            {
-              "fieldLabel": "Пароль",
-              "fieldName": "password",
-              "fieldType": "password",
-              "ref": "passwordInput"
-            }
-          ],
-          "button": {
-            "type": "submit",
-            "text": "Войти"
-          },
-          "subButton": "Нет аккаунта?"
-        }'}}}
+        <div class='form-container-wrapper'>
+          <div class='form-container'>
+            <h2 class='form-container__header'>Вход</h2>
+            {{{ FormContainer data='{
+              "fields": [
+                {
+                  "fieldLabel": "Логин",
+                  "fieldName": "login",
+                  "fieldType": "text",
+                  "ref": "loginInput"
+                },
+                {
+                  "fieldLabel": "Пароль",
+                  "fieldName": "password",
+                  "fieldType": "password",
+                  "ref": "passwordInput"
+                }
+              ],
+              "button": {
+                "type": "submit",
+                "text": "Войти"
+              },
+              "subButton": "Нет аккаунта?"
+            }'}}}
+            {{{ ButtonLink text="Нет аккаунта?" className='form-container__link' onClick=onClick }}}
+          </div>
+        </div>  
       </div>
     `;
   }
 }
+
+export default withRouter(SignInPage);
