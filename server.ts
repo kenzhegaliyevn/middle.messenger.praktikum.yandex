45fe694b702d04c16441b6b19112969a47d24ffe
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2023-02-28 17:19:30 +0500

    fix

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./dist/'));
+app.use(express.static("dist"));
 
-app.listen(PORT, () => {
-  console.log(`My Chat listening on port ${PORT}!`);
+app.listen(port, () => {
+    console.log(`listen port ${port}`);
 });
